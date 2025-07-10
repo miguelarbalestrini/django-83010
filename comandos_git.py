4f44f9dafa19d684d8fe5d761aeffd5df9c90de1
Miguel Rodenas <migue@example.com>
2025-07-10 19:51:59 -0300

    agregamos comentarios con los comandos

---
+++ comandos_git.py
@@ -0,0 +1,15 @@
+# Comandos para configurar nuestra identidad:
+
+# git config --global user.name "nombre apellido"
+# git config --global user.email "nombre@example.com"
+
+# git init  Se usa una sola vez (crea capeta .git)
+
+
+# Se usan cuando nosotros queramos:
+# git status  Muestra el estado del repo (archivos modificados, no agregados, etc.)
+# git log  Muestra el historial de commits
+
+# Se repiten siempre que haya cambios:
+# git add .  Agrega todos los archivos al repo
+# git commit -m "mensaje"  Crea un commit con los archivos agregados (etiqueta de info)
